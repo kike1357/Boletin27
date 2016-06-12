d323f7625fdeb5fa5f118c6e9cab1558d32a039a
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-12 16:18:07 +0200

    commiit

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,12 @@
+package boletin27;
+
+
+public class Boletin27 {
+
+    
+    public static void main(String[] args) {
+        Ventana vent=new Ventana();
+        vent.setVisible(true);
+    }
+    
+}
